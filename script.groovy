fd9459cb51964d82cf5f7b346ba03f62ed3e94bc
Omar Riad <omarriad22@gmail.com>
2024-05-03 21:56:18 +0300

    update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage(){
     withCredentials([usernamePassword(credentialsId: "dockerhub-credentials", passwordVariable: "PASS", usernameVariable:"USER")]){
     sh 'docker build -t omarriad07/demo-app:jma-2.0 .'
     sh 'echo $PASS | docker login -u $USER --password-stdin'
-    sh 'dokcer push omarriad07/demo-app:jma-2.0'
+    sh 'docker push omarriad07/demo-app:jma-2.0'
     }
 }
 
